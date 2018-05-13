b3a43e6bd0043293b4d71acee865511542752da4
Suleyman Poyraz <zaryob.dev@gmail.com>
2018-05-13 22:11:46 +0300

    Initial commit

---
+++ create-archive.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NAME=baselayout
+VERSION=3.10.1
+
+DIRNAME=$NAME-$VERSION
+
+if [ -d ../$DIRNAME ]; then
+    echo ../$DIRNAME already exists. Remove it first.
+    exit 1
+fi
+
+cp -r ../$NAME ../$DIRNAME
+tar czvf $DIRNAME.tar.gz ../$DIRNAME --exclude .git --exclude .svn --exclude `basename $0`
+rm -rf ../$DIRNAME
